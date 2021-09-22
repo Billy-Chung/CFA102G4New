d5cdfba74beb7598c8f84d7ded1563e6d8470c36
Amos Chung <zxcy9980@gmail.com>
2021-09-22 21:50:40 +0800

    擋住登出後的上一頁

--- src/com/filters/LoginFilter.java
+++ src/com/filters/LoginFilter.java
@@ -30,6 +30,11 @@ public class LoginFilter implements Filter {
 		HttpSession session = req.getSession();
 		Object admin = session.getAttribute("admin");
 		
+		res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+		res.setDateHeader("Expires", 0); // Proxies.
+
+		
 		if(admin == null) {
 			session.setAttribute("location", req.getRequestURI());
 			res.sendRedirect(req.getContextPath()+"/back_end/login/login.jsp");
