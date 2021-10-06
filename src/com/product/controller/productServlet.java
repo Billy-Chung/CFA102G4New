64aa805a859d420139d3406205ccbee01adc1e85
Amos Chung <zxcy9980@gmail.com>
2021-10-06 16:16:26 +0800

    更改大小寫

--- src/com/product/controller/productServlet.java
+++ src/com/product/controller/productServlet.java
@@ -1,15 +1,18 @@
 package com.product.controller;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.product.model.productService;
+import com.productPhotos.model.productPhotosService;
 import com.product.model.ProductVO;
 
 public class productServlet extends HttpServlet {
